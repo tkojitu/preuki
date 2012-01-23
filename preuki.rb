b06ca1b1798c7f4092c392083aa4cf2e2af2e8b2
Tomoyuki Kosimizu <tko@jitu.org>
2012-01-23 23:54:59 +0900

    on_edit.

--- preuki.rb
+++ preuki.rb
@@ -6,28 +6,41 @@ class Preuki < WEBrick::CGI
   PAGE_ROOT = "/media/4GB/httpd/preuki/"
 
   def do_GET(req, res)
+    res["Content-Type"] = "text/html"
     if req.query.has_key?("view")
       on_view(req, res)
+    elsif req.query.has_key?("edit")
+      on_edit(req, res)
     else
       on_else(req, res)
     end
   end
 
   def on_view(req, res)
-    res["Content-Type"] = "text/html"
     begin
       res.body = "<html><body><pre>"
-      text = File.read(PAGE_ROOT + req.query["view"]) # insesure
+      text = File.read(PAGE_ROOT + req.query["view"])
       res.body << text
       res.body << "</pre></body></html>"
     rescue
       on_else(req, res)
-      return
+    end
+  end
+
+  def on_edit(req, res)
+    begin
+      res.body = "<html><body><form method='POST' action='?save=%s'>\n" % req.query["edit"]
+      res.body << "<input type='submit' value='Save'><br>\n"
+      res.body << "<textarea name='text' style='width=100%' rows=25 cols=80 wrap='virtual'>"
+      text = File.read(PAGE_ROOT + req.query["edit"])
+      res.body << text
+      res.body << "</textarea></form></body></html>"
+    rescue
+      on_else(req, res)
     end
   end
 
   def on_else(req, res)
-    res["Content-Type"] = "text/html"
     res.body = "<html><body><p>hello, world</p></body></html>"
   end
 end
