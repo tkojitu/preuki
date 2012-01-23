62d17e54a7d48f8bf5d6d9d90375bc7f80c1560e
Tomoyuki Kosimizu <tko@jitu.org>
2012-01-23 23:28:17 +0900

    on_view.

--- preuki.rb
+++ preuki.rb
@@ -3,7 +3,30 @@
 require 'webrick/cgi'
 
 class Preuki < WEBrick::CGI
+  PAGE_ROOT = "/media/4GB/httpd/preuki/"
+
   def do_GET(req, res)
+    if req.query.has_key?("view")
+      on_view(req, res)
+    else
+      on_else(req, res)
+    end
+  end
+
+  def on_view(req, res)
+    res["Content-Type"] = "text/html"
+    begin
+      res.body = "<html><body><pre>"
+      text = File.read(PAGE_ROOT + req.query["view"]) # insesure
+      res.body << text
+      res.body << "</pre></body></html>"
+    rescue
+      on_else(req, res)
+      return
+    end
+  end
+
+  def on_else(req, res)
     res["Content-Type"] = "text/html"
     res.body = "<html><body><p>hello, world</p></body></html>"
   end
