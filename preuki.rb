f3d4065d603c67b28c868a58353626ddc02c7b7c
Tomoyuki Kosimizu <tko@jitu.org>
2012-01-24 01:43:20 +0900

    do_POST.

--- preuki.rb
+++ preuki.rb
@@ -5,6 +5,10 @@ require 'webrick/cgi'
 class Preuki < WEBrick::CGI
   PAGE_ROOT = "/media/4GB/httpd/preuki/"
 
+  def initialize
+    super()
+  end
+
   def do_GET(req, res)
     res["Content-Type"] = "text/html"
     if req.query.has_key?("view")
@@ -18,15 +22,19 @@ class Preuki < WEBrick::CGI
 
   def on_view(req, res)
     begin
-      res.body = "<html><body><pre>"
-      text = File.read(PAGE_ROOT + req.query["view"])
-      res.body << text
-      res.body << "</pre><hr>\n"
-      res.body << ("<a href='?edit=%s'>EditText</a>\n" % req.query["view"])
-      res.body << "</body></html>"
+      show_page(res, req.query["view"])
     rescue
       on_else(req, res)
     end
+  end
+
+  def show_page(res, pagename)
+    res.body = "<html><body><pre>"
+    text = File.read(PAGE_ROOT + pagename)
+    res.body << text
+    res.body << "</pre><hr>\n"
+    res.body << ("<a href='?edit=%s'>EditText</a>\n" % pagename)
+    res.body << "</body></html>"
   end
 
   def on_edit(req, res)
@@ -45,6 +53,33 @@ class Preuki < WEBrick::CGI
 
   def on_else(req, res)
     res.body = "<html><body><p>hello, world</p></body></html>"
+  end
+
+  def do_POST(req, res)
+    res["Content-Type"] = "text/html"
+    on_else(req, res)
+    if /\Asave=/ =~ req.query_string
+      on_save(req, res)
+    else
+      on_else(req, res)
+    end
+  end
+
+  def on_save(req, res)
+    begin
+      pagename = req.query_string.sub(/\Asave=/, '')
+      save_page(req, pagename)
+      show_page(res, pagename)
+    rescue
+      logger.error($!)
+      on_else(req, res)
+    end
+  end
+
+  def save_page(req, pagename)
+    File.open(PAGE_ROOT + pagename, "w") do |output|
+      output.print(req.query["text"])
+    end
   end
 end
 
