ccfcb23615051e537ee7660cb38e2542e3c14342
Tomoyuki Kosimizu <tko@jitu.org>
2012-01-31 10:27:40 +0900

    module.

--- preuki.rb
+++ preuki.rb
@@ -2,86 +2,88 @@
 
 require 'webrick/cgi'
 
-class Preuki < WEBrick::CGI
-  PAGE_ROOT = "/media/4GB/httpd/preuki/"
+module Preuki
+  class PreukiCGI < WEBrick::CGI
+    PAGE_ROOT = "/media/4GB/httpd/preuki/"
 
-  def initialize
-    super()
-  end
-
-  def do_GET(req, res)
-    res["Content-Type"] = "text/html"
-    if req.query.has_key?("view")
-      on_view(req, res)
-    elsif req.query.has_key?("edit")
-      on_edit(req, res)
-    else
-      on_else(req, res)
+    def initialize
+      super()
     end
-  end
 
-  def on_view(req, res)
-    begin
-      show_page(res, req.query["view"])
-    rescue
-      on_else(req, res)
+    def do_GET(req, res)
+      res["Content-Type"] = "text/html"
+      if req.query.has_key?("view")
+        on_view(req, res)
+      elsif req.query.has_key?("edit")
+        on_edit(req, res)
+      else
+        on_else(req, res)
+      end
     end
-  end
 
-  def show_page(res, pagename)
-    res.body = "<html><body><pre>"
-    text = File.read(PAGE_ROOT + pagename)
-    res.body << text
-    res.body << "</pre><hr>\n"
-    res.body << ("<a href='?edit=%s'>EditText</a>\n" % pagename)
-    res.body << "</body></html>"
-  end
+    def on_view(req, res)
+      begin
+        show_page(res, req.query["view"])
+      rescue
+        on_else(req, res)
+      end
+    end
 
-  def on_edit(req, res)
-    begin
-      res.body = "<html><body>\n"
-      res.body << "<form method='POST' action='?'>\n"
-      res.body << "<input type='submit' value='Save'><br>\n"
-      res.body << "<textarea name='text' style='width:100%;height:90%' wrap='virtual'>"
-      text = File.read(PAGE_ROOT + req.query["edit"])
+    def show_page(res, pagename)
+      res.body = "<html><body><pre>"
+      text = File.read(PAGE_ROOT + pagename)
       res.body << text
-      res.body << "</textarea>\n"
-      res.body << ("<input type='hidden' name='save' value='%s'>\n" % req.query["edit"])
-      res.body << "</form></body></html>"
-    rescue
-      on_else(req, res)
+      res.body << "</pre><hr>\n"
+      res.body << ("<a href='?edit=%s'>EditText</a>\n" % pagename)
+      res.body << "</body></html>"
     end
-  end
 
-  def on_else(req, res)
-    res.body = "<html><body><p>hello, world</p></body></html>"
-  end
-
-  def do_POST(req, res)
-    res["Content-Type"] = "text/html"
-    if req.query.has_key?("save")
-      on_save(req, res)
-    else
-      on_else(req, res)
+    def on_edit(req, res)
+      begin
+        res.body = "<html><body>\n"
+        res.body << "<form method='POST' action='?'>\n"
+        res.body << "<input type='submit' value='Save'><br>\n"
+        res.body << "<textarea name='text' style='width:100%;height:90%' wrap='virtual'>"
+        text = File.read(PAGE_ROOT + req.query["edit"])
+        res.body << text
+        res.body << "</textarea>\n"
+        res.body << ("<input type='hidden' name='save' value='%s'>\n" % req.query["edit"])
+        res.body << "</form></body></html>"
+      rescue
+        on_else(req, res)
+      end
     end
-  end
 
-  def on_save(req, res)
-    begin
-      pagename = req.query["save"]
-      save_page(req, pagename)
-      res.set_redirect(WEBrick::HTTPStatus::Found, "?view=%s" % pagename)
-    rescue
-      logger.error($!)
-      on_else(req, res)
+    def on_else(req, res)
+      res.body = "<html><body><p>hello, world</p></body></html>"
     end
-  end
 
-  def save_page(req, pagename)
-    File.open(PAGE_ROOT + pagename, "w") do |output|
-      output.print(req.query["text"])
+    def do_POST(req, res)
+      res["Content-Type"] = "text/html"
+      if req.query.has_key?("save")
+        on_save(req, res)
+      else
+        on_else(req, res)
+      end
+    end
+
+    def on_save(req, res)
+      begin
+        pagename = req.query["save"]
+        save_page(req, pagename)
+        res.set_redirect(WEBrick::HTTPStatus::Found, "?view=%s" % pagename)
+      rescue
+        logger.error($!)
+        on_else(req, res)
+      end
+    end
+
+    def save_page(req, pagename)
+      File.open(PAGE_ROOT + pagename, "w") do |output|
+        output.print(req.query["text"])
+      end
     end
   end
 end
 
-Preuki.new.start
+Preuki::PreukiCGI.new.start
