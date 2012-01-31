b7d35b10102f0cbd6231548e02a9b4d4126c6dd6
Tomoyuki Kosimizu <tko@jitu.org>
2012-01-31 16:26:42 +0900

    get_command.

--- preuki.rb
+++ preuki.rb
@@ -12,52 +12,75 @@ module Preuki
 
     def do_GET(req, res)
       res["Content-Type"] = "text/html"
-      if req.query.has_key?("view")
-        on_view(req, res)
-      elsif req.query.has_key?("edit")
-        on_edit(req, res)
+      cmd, page = get_command(req)
+      case cmd
+      when "view"
+        on_view(page, res)
+      when "edit"
+        on_edit(page, res)
       else
-        on_else(req, res)
+        on_else(page, res)
       end
     end
 
-    def on_view(req, res)
+    def get_command(req)
+      key = command_key(req)
+      return [nil, nil] unless key
+      return [key, get_page(req, key)]
+    end
+
+    def command_key(req)
+      if req.query.has_key?("view")
+        return "view"
+      elsif req.query.has_key?("edit")
+        return "edit"
+      elsif req.query.has_key?("save")
+        return "save"
+      else
+        return nil
+      end
+    end
+
+    def get_page(req, key)
+      return WEBrick::HTMLUtils::escape(req.query[key]).gsub(/\.\.|\/|\\|:/, '')
+    end
+
+    def on_view(page, res)
       begin
-        show_page(res, req.query["view"])
+        show_page(page, res)
       rescue Errno::ENOENT
-        set_redirect_to_new_page_editor(req, res)
+        set_redirect_to_new_page_editor(page, res)
       rescue
-        on_else(req, res)
+        on_else(page, res)
       end
     end
 
-    def show_page(res, pagename)
+    def show_page(page, res)
       res.body = "<html><body><pre>"
-      text = File.read(PAGE_ROOT + pagename)
+      text = File.read(PAGE_ROOT + page)
       Notation.new.format!(text)
       res.body << text
       res.body << "</pre><hr>\n"
-      res.body << ("<a href='?edit=%s'>EditText</a>\n" % pagename)
+      res.body << ("<a href='?edit=%s'>EditText</a>\n" % page)
       res.body << "</body></html>"
     end
 
-    def set_redirect_to_new_page_editor(req, res)
-        res.set_redirect(WEBrick::HTTPStatus::SeeOther,
-                         "?edit=%s" % req.query["view"])
+    def set_redirect_to_new_page_editor(page, res)
+        res.set_redirect(WEBrick::HTTPStatus::SeeOther, "?edit=%s" % page)
     end
 
-    def on_edit(req, res)
+    def on_edit(page, res)
       begin
-        text = File.read(PAGE_ROOT + req.query["edit"])
-        set_editor(res, text, req.query["edit"])
+        text = File.read(PAGE_ROOT + page)
+        set_editor(page, res, text)
       rescue Errno::ENOENT
-        set_new_page_editor(req, res)
+        set_new_page_editor(page, res)
       rescue
-        on_else(req, res)
+        on_else(page, res)
       end
     end
 
-    def set_editor(res, text, page)
+    def set_editor(page, res, text)
         res.body = "<html><body>\n"
         res.body << "<form method='POST' action='?'>\n"
         res.body << "<input type='submit' value='Save'><br>\n"
@@ -68,36 +91,37 @@ module Preuki
         res.body << "</form></body></html>"
     end
 
-    def set_new_page_editor(req, res)
-      set_editor(res, "", req.query["edit"])
+    def set_new_page_editor(page, res)
+      set_editor(page, res, "")
     end
 
-    def on_else(req, res)
+    def on_else(page, res)
       res.body = "<html><body><p>hello, world</p></body></html>"
     end
 
     def do_POST(req, res)
       res["Content-Type"] = "text/html"
-      if req.query.has_key?("save")
-        on_save(req, res)
+      cmd, page = get_command(req)
+      case cmd
+      when "save"
+        on_save(page, req, res)
       else
-        on_else(req, res)
+        on_else(page, res)
       end
     end
 
-    def on_save(req, res)
+    def on_save(page, req, res)
       begin
-        pagename = req.query["save"]
-        save_page(req, pagename)
-        res.set_redirect(WEBrick::HTTPStatus::SeeOther, "?view=%s" % pagename)
+        save_page(page, req)
+        res.set_redirect(WEBrick::HTTPStatus::SeeOther, "?view=%s" % page)
       rescue
         logger.error($!)
-        on_else(req, res)
+        on_else(page, res)
       end
     end
 
-    def save_page(req, pagename)
-      File.open(PAGE_ROOT + pagename, "w") do |output|
+    def save_page(page, req)
+      File.open(PAGE_ROOT + page, "w") do |output|
         output.print(req.query["text"])
       end
     end
